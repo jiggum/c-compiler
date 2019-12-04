03b686673bce1a0dea5d77bff0b8af01b8688a21
min <0s.dongmin@gmail.com>
2019-12-04 21:33:47 +0900

    add parent member, BaseSection, ArrayNode

--- ast.py
+++ ast.py
@@ -1,29 +1,63 @@
+import symbol_table as st
+
 class Node():
-  def __init__(self, child=None, lineno=None):
-    self.childs = []
+  def __init__(self, lineno=None):
     self.lineno = lineno
-    if child != None:
-      self.childs.append(child)
-
-  def is_leaf(self):
-      return len(self.childs) == 0
-
-  def add(self, child):
-    self.childs.append(child)
+    self.parent = None
+    self.callstack = None
+    self.scope = None
+    self.current_line = None
+    self.section_lineno = None
 
   def is_empty(self):
     return False
 
+  def get_scope(self):
+    self.parent.get_scope()
+
+  def get_callstack(self):
+    return self.parent.get_callstack()
+
+  def get_current_line(self):
+    return self.parent.get_current_line()
+
+  def update_current_line(self, line):
+    self.parent.update_current_line(line)
+
+  def get_section_lineno(self):
+    return self.parent.get_section_lineno()
+
+  def push_callstack(self, node):
+    return self.parent.push_callstack(node)
+
+  def pop_callstack(self):
+    return self.parent.pop_callstack()
+
   def __str__(self, level=0, infos={}):
     ret = "\t"*level + str(self.lineno) + ':' + self.__class__.__name__ + infos.__str__() + '\n'
+    return ret
+
+class ArrayNode(Node):
+  def __init__(self, child=None, lineno=None):
+    super().__init__(lineno=lineno)
+    self.childs = []
+    if child != None:
+      child.parent = self
+      self.childs.append(child)
+
+  def add(self, child):
+    child.parent = self
+    self.childs.append(child)
+
+
+  def __str__(self, level=0, infos={}):
+    ret = super().__str__(level, infos)
     for child in self.childs:
         ret += child.__str__(level+1)
     return ret
 
-class EmptyNode(Node):
-  def __init__(self):
-    super().__init__()
 
+class EmptyNode(Node):
   def is_empty(self):
     return True
 
@@ -57,18 +91,50 @@ class Const(Node):
     infos_.update(infos)
     return super().__str__(level, infos_)
 
-class RootSection(Node):
+class BaseSection(ArrayNode):
+  def __init__(self, child=None, lineno=None):
+    super().__init__(child=child, lineno=lineno)
+    self.current_line = 1
+
+  def get_current_line(self):
+    return self.current_line
+
+  def get_section_lineno(self):
+    return self.lineno
+
+  def update_current_line(self, line):
+    self.current_line = line
+
+  def pop_callstack(self):
+    return self.callstack.pop()
+
+class RootSection(BaseSection):
+  def __init__(self, child=None, lineno=None):
+    super().__init__(child=child, lineno=lineno)
+    self.callstack = []
+
+  def get_scope(self):
+    return self.scope
+
+  def get_callstack(self):
+    return self.callstack
+
+  def push_callstack(self, node):
+    self.callstack.append(node)
+
+class ScopelessSection(BaseSection):
   pass
 
-class Section(Node):
-  pass
+class Section(BaseSection):
+  def get_scope(self):
+    return self.scope
 
 class Declaration(Node):
   def __init__(self, declarator, lineno=None):
     super().__init__(lineno=lineno)
     self.type = declarator.type
+    self.type.parent = self
     self.name = declarator.name
-
 
   def __str__(self, level=0, infos={}):
     infos_ = {'name': self.name}
@@ -81,7 +147,9 @@ class FnDeclaration(Declaration):
   def __init__(self, declarator, body, lineno=None):
     super().__init__(declarator, lineno=lineno)
     self.parameterGroup = declarator.parameterGroup
+    self.parameterGroup.parent = self
     self.body = body
+    self.body.parent = self
 
 
   def __str__(self, level=0, infos={}):
@@ -93,7 +161,7 @@ class FnDeclaration(Declaration):
 class VaDeclartion(Declaration):
   pass
 
-class VaDeclationList(Node):
+class VaDeclationList(ArrayNode):
   pass
 
 class Declarator(Node):
@@ -105,6 +173,7 @@ class Declarator(Node):
   def add_type(self, typeNode):
     if self.type is None:
       self.type = typeNode
+      self.type.parent = self
     else:
       self.type.add_type(typeNode.get_type())
 
@@ -119,6 +188,7 @@ class FnDeclarator(Declarator):
   def __init__(self, name, parameterGroup=None, lineno=None):
     super().__init__(name, lineno=lineno)
     self.parameterGroup = parameterGroup
+    self.parameterGroup.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
@@ -138,15 +208,18 @@ class ArrayDeclarator(Declarator):
     infos_.update(infos)
     return super().__str__(level, infos_)
 
-class ParameterGroup(Node):
+class ParameterGroup(ArrayNode):
   pass
 
 class ConditionalStatement(Node):
   def __init__(self, expr, then_section, else_section, lineno=None):
     super().__init__(lineno=lineno)
     self.expr = expr
+    self.expr.parent = self
     self.then_section = then_section
+    self.then_section.parent = self
     self.else_section = else_section
+    self.else_section.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
@@ -159,7 +232,9 @@ class LoopStatement(Node):
   def __init__(self, expr, section, lineno=None):
     super().__init__(lineno=lineno)
     self.expr = expr
+    self.expr.parent = self
     self.section = section
+    self.section.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
@@ -174,7 +249,9 @@ class For(LoopStatement):
   def __init__(self, init_stmt, expr, term_stmt, section, lineno=None):
     super().__init__(expr, section, lineno=lineno)
     self.init_stmt = init_stmt
+    self.init_stmt.parent = self
     self.term_stmt = term_stmt
+    self.term_stmt.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
@@ -189,6 +266,7 @@ class Return(JumpStatement):
   def __init__(self, expr, lineno=None):
     super().__init__(lineno=lineno)
     self.expr = expr
+    self.expr.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
@@ -201,14 +279,16 @@ class Break(JumpStatement):
 class Continue(JumpStatement):
   pass
 
-class ArgumentList(Node):
+class ArgumentList(ArrayNode):
   pass
 
 class BinaryOp(Node):
   def __init__(self, op, left, right, lineno=None):
     super().__init__(lineno=lineno)
     self.left = left
+    self.left.parent = self
     self.right = right
+    self.right.parent = self
     self.op = op
 
   def __str__(self, level=0, infos={}):
@@ -226,6 +306,7 @@ class UnaryOp(Node):
   def __init__(self, op, expr, lineno=None):
     super().__init__(lineno=lineno)
     self.expr = expr
+    self.expr.parent = self
     self.op = op
 
   def __str__(self, level=0, infos={}):
@@ -239,7 +320,9 @@ class FnExpression(Node):
   def __init__(self, expr, arguments, lineno=None):
     super().__init__(lineno=lineno)
     self.expr = expr
+    self.expr.parent = self
     self.arguments = arguments
+    self.arguments.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
@@ -265,7 +348,9 @@ class ArrayExpression(Node):
   def __init__(self, expr, index, lineno=None):
     super().__init__(lineno=lineno)
     self.expr = expr
+    self.expr.parent = self
     self.index = index
+    self.index.parent = self
 
   def __str__(self, level=0, infos={}):
     ret = super().__str__(level, infos)
--- parser.py
+++ parser.py
@@ -50,7 +50,7 @@ class Parser(Lexer):
 
   def p_root_section_1(self, t):
     '''root_section : root_sector'''
-    t[0] = ast.RootSection(t[1])
+    t[0] = ast.RootSection(t[1], lineno=t[1].lineno)
 
   def p_root_section_2(self, t):
     '''root_section : root_section root_sector'''
@@ -78,10 +78,31 @@ class Parser(Lexer):
 
   def p_section_1(self, t):
     '''section : sector'''
-    t[0] = ast.Section(t[1])
+    t[0] = ast.Section(t[1], lineno=t[1].lineno)
 
   def p_section_2(self, t):
     '''section : section sector'''
+    t[1].add(t[2])
+    t[0] = t[1]
+
+  def p_scopeless_section_wrapper(self, t):
+    '''scopeless_section_wrapper : BRACE_L scopeless_section_opt BRACE_R'''
+    t[0] = t[2]
+
+  def p_scopeless_section_opt_1(self, t):
+    '''scopeless_section_opt : empty'''
+    pass
+
+  def p_scopeless_section_opt_2(self, t):
+    '''scopeless_section_opt : scopeless_section'''
+    t[0] = t[1]
+
+  def p_scopeless_section_1(self, t):
+    '''scopeless_section : sector'''
+    t[0] = ast.ScopelessSection(t[1], lineno=t[1].lineno)
+
+  def p_scopeless_section_2(self, t):
+    '''scopeless_section : scopeless_section sector'''
     t[1].add(t[2])
     t[0] = t[1]
 
@@ -100,7 +121,7 @@ class Parser(Lexer):
   def p_function_declaration(self, t):
     '''function_declaration : type function_declarator section_wrapper'''
     t[2].add_type(t[1])
-    t[0] = ast.FnDeclaration(t[2], t[3])
+    t[0] = ast.FnDeclaration(t[2], t[3], lineno=t[1].lineno)
 
   def p_function_declarator_1(self, t):
     '''function_declarator : function_simple_declarator'''
@@ -108,21 +129,21 @@ class Parser(Lexer):
 
   def p_function_declarator_2(self, t):
     '''function_declarator : ASTERISK function_declarator'''
-    t[2].add_type(ast.TypeNode(t[1]))
+    t[2].add_type(ast.TypeNode(t[1], lineno=t.lineno(1)))
     t[0] = t[2]
 
   def p_function_simple_declarator_1(self, t):
     '''function_simple_declarator : ID PAREN_L parameter_group PAREN_R'''
-    t[0] = ast.FnDeclarator(t[1], t[3])
+    t[0] = ast.FnDeclarator(t[1], t[3], lineno=t.lineno(1))
 
   def p_function_simple_declarator_2(self, t):
     '''function_simple_declarator : ID PAREN_L PAREN_R
                                   | ID PAREN_L VOID PAREN_R'''
-    t[0] = ast.FnDeclarator(t[1], ast.EmptyNode())
+    t[0] = ast.FnDeclarator(t[1], ast.EmptyNode(), lineno=t.lineno(1))
 
   def p_variable_declaration(self, t):
     '''variable_declaration : type variable_declarator_group SEMICOLON'''
-    vdl = ast.VaDeclationList()
+    vdl = ast.VaDeclationList(lineno=t[1].lineno)
     for variable_declarator in t[2]:
       variable_declarator.add_type(t[1])
       vdl.add(variable_declarator)
@@ -143,21 +164,21 @@ class Parser(Lexer):
 
   def p_variable_declarator_2(self, t):
     '''variable_declarator : ASTERISK variable_declarator'''
-    t[2].add_type(ast.TypeNode(t[1]))
+    t[2].add_type(ast.TypeNode(t[1], lineno=t.lineno(1)))
     t[0] = t[2]
 
   def p_variable_simple_declarator_1(self, t):
     '''variable_simple_declarator : ID'''
-    t[0] = ast.VaDeclarator(t[1])
+    t[0] = ast.VaDeclarator(t[1], lineno=t.lineno(1))
 
   def p_variable_simple_declarator_2(self, t):
     '''variable_simple_declarator : ID BRACKET_L NUMBER_INT BRACKET_R'''
-    t[0] = ast.ArrayDeclarator(t[1], t[3])
+    t[0] = ast.ArrayDeclarator(t[1], t[3], lineno=t.lineno(1))
 
   def p_parameter_group_1(self, t):
     '''parameter_group : type variable_declarator'''
     t[2].add_type(t[1])
-    t[0] = ast.ParameterGroup(t[2])
+    t[0] = ast.ParameterGroup(t[2], lineno=t[1].lineno)
 
   def p_parameter_group_2(self, t):
     '''parameter_group : parameter_group COMMA type variable_declarator'''
@@ -180,41 +201,41 @@ class Parser(Lexer):
 
   def p_conditional_statement_1(self, t):
     '''conditional_statement : IF PAREN_L expression PAREN_R section_wrapper %prec IFS'''
-    t[0] = ast.ConditionalStatement(t[3], t[5], ast.EmptyNode())
+    t[0] = ast.ConditionalStatement(t[3], t[5], ast.EmptyNode(), lineno=t.lineno(1))
 
   def p_conditional_statement_2(self, t):
     '''conditional_statement : IF PAREN_L expression PAREN_R section_wrapper ELSE section_wrapper'''
-    t[0] = ast.ConditionalStatement(t[3], t[5], t[7])
+    t[0] = ast.ConditionalStatement(t[3], t[5], t[7], lineno=t.lineno(1))
 
   def p_loop_statement_1(self, t):
     '''loop_statement : WHILE PAREN_L expression PAREN_R section_wrapper'''
-    t[0] = ast.While(t[3], t[5])
+    t[0] = ast.While(t[3], t[5], lineno=t.lineno(1))
 
   def p_loop_statement_2(self, t):
-    '''loop_statement : FOR PAREN_L expression_statement expression_statement expression PAREN_R section_wrapper'''
-    t[0] = ast.For(t[3], t[4], t[5], t[7])
+    '''loop_statement : FOR PAREN_L expression_statement expression_statement expression PAREN_R scopeless_section_wrapper'''
+    t[0] = ast.For(t[3], t[4], t[5], t[7], lineno=t.lineno(1))
 
   def p_jump_statement_1(self, t):
     '''jump_statement : RETURN SEMICOLON'''
-    t[0] = ast.Return(ast.EmptyNode())
+    t[0] = ast.Return(ast.EmptyNode(), lineno=t.lineno(1))
 
   def p_jump_statement_2(self, t):
     '''jump_statement : RETURN expression SEMICOLON'''
-    t[0] = ast.Return(t[2])
+    t[0] = ast.Return(t[2], lineno=t.lineno(1))
 
   def p_jump_statement_3(self, t):
     '''jump_statement : BREAK SEMICOLON'''
-    t[0] = ast.Break()
+    t[0] = ast.Break(lineno=t.lineno(1))
 
   def p_jump_statement_4(self, t):
     '''jump_statement : CONTINUE SEMICOLON'''
-    t[0] = ast.Continue()
+    t[0] = ast.Continue(lineno=t.lineno(1))
 
   # Expression
 
   def p_argument_expression_list_1(self, t):
     '''argument_expression_list : expression'''
-    t[0] = ast.ArgumentList(t[1])
+    t[0] = ast.ArgumentList(t[1], lineno=t[1].lineno)
 
   def p_argument_expression_list_2(self, t):
     '''argument_expression_list : argument_expression_list COMMA expression'''
@@ -233,7 +254,7 @@ class Parser(Lexer):
                          | variable_expression ASSIGN_TIME expression
                          | variable_expression ASSIGN_DIVIDE expression
                          | variable_expression ASSIGN_MODULO expression'''
-    t[0] = ast.AssignOp(t[2], t[1], t[3])
+    t[0] = ast.AssignOp(t[2], t[1], t[3], lineno=t[1].lineno)
 
   def p_additive_expression_1(self, t):
     '''additive_expression : unary_expression'''
@@ -253,7 +274,7 @@ class Parser(Lexer):
                            | additive_expression ASTERISK unary_expression
                            | additive_expression DIVIDE unary_expression
                            | additive_expression MODULO unary_expression'''
-    t[0] = ast.BinaryOp(t[2], t[1], t[3])
+    t[0] = ast.BinaryOp(t[2], t[1], t[3], lineno=t[1].lineno)
 
   def p_unary_expression_1(self, t):
     '''unary_expression : function_expression
@@ -263,17 +284,17 @@ class Parser(Lexer):
   def p_unary_expression_2(self, t):
     '''unary_expression : MINUS unary_expression
                         | PLUS unary_expression'''
-    t[0] = ast.UnaryOp(t[1], t[2])
+    t[0] = ast.UnaryOp(t[1], t[2], lineno=t.lineno(1))
 
   def p_unary_expression_3(self, t):
     '''unary_expression : function_expression DOUBLE_PLUS
                         | function_expression DOUBLE_MINUS'''
-    t[0] = ast.BinaryOp(t[2], t[1], ast.Const(1))
+    t[0] = ast.BinaryOp(t[2], t[1], ast.Const(1), lineno=t[1].lineno)
 
   def p_unary_expression_4(self, t):
     '''unary_expression : DOUBLE_PLUS function_expression
                         | DOUBLE_MINUS function_expression'''
-    t[0] = ast.BinaryOp(t[1], t[2], ast.Const(1))
+    t[0] = ast.BinaryOp(t[1], t[2], ast.Const(1), lineno=t.lineno(1))
 
   def p_function_expression_1(self, t):
     '''function_expression : variable_expression'''
@@ -281,11 +302,11 @@ class Parser(Lexer):
 
   def p_function_expression_2(self, t):
     '''function_expression : function_expression PAREN_L argument_expression_list PAREN_R'''
-    t[0] = ast.FnExpression(t[1], t[3])
+    t[0] = ast.FnExpression(t[1], t[3], lineno=t[1].lineno)
 
   def p_function_expression_3(self, t):
     '''function_expression : function_expression PAREN_L PAREN_R'''
-    t[0] = ast.FnExpression(t[1], ast.EmptyNode)
+    t[0] = ast.FnExpression(t[1], ast.EmptyNode, lineno=t[1].lineno)
 
   def p_variable_expression_1(self, t):
     '''variable_expression : variable_simpe_expression'''
@@ -298,11 +319,11 @@ class Parser(Lexer):
 
   def p_variable_simpe_expression_1(self, t):
     '''variable_simpe_expression : ID'''
-    t[0] = ast.VaExpression(t[1])
+    t[0] = ast.VaExpression(t[1], lineno=t.lineno(1))
 
   def p_variable_simpe_expression_2(self, t):
     '''variable_simpe_expression : variable_simpe_expression BRACKET_L expression BRACKET_R'''
-    t[0] = ast.ArrayExpression(t[1], t[3])
+    t[0] = ast.ArrayExpression(t[1], t[3], lineno=t[1].lineno)
 
   def p_variable_simpe_expression_3(self, t):
     '''variable_simpe_expression : PAREN_L expression PAREN_R'''
@@ -310,19 +331,19 @@ class Parser(Lexer):
 
   def p_const_expression_1(self, t):
     '''const_expression : NUMBER_FLOAT'''
-    t[0] = ast.Const(float(t[1]))
+    t[0] = ast.Const(float(t[1]), lineno=t.lineno(1))
 
   def p_const_expression_2(self, t):
     '''const_expression : NUMBER_INT'''
-    t[0] = ast.Const(int(t[1]))
+    t[0] = ast.Const(int(t[1]), lineno=t.lineno(1))
 
   def p_const_expression_3(self, t):
     '''const_expression : CHARACTER'''
-    t[0] = ast.Const(t[1])
+    t[0] = ast.Const(t[1], lineno=t.lineno(1))
 
   def p_const_expression_4(self, t):
     '''const_expression : STRING'''
-    t[0] = ast.Const(t[1])
+    t[0] = ast.Const(t[1], lineno=t.lineno(1))
 
   # ETC
 
@@ -331,7 +352,7 @@ class Parser(Lexer):
             | FLOAT
             | VOID
             | CHAR'''
-    t[0] = ast.TypeNode(t[1])
+    t[0] = ast.TypeNode(t[1], lineno=t.lineno(1))
 
   def p_empty(self, t):
     'empty :'
