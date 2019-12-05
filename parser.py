0d80d68948a6f8ce6fa0865d7df1fb5626f51dda
min <0s.dongmin@gmail.com>
2019-12-05 15:10:01 +0900

    trim all linespan to tuple from array

--- parser.py
+++ parser.py
@@ -54,7 +54,7 @@ class Parser(Lexer):
 
   def p_root_section_2(self, p):
     '''root_section : root_section root_sector'''
-    linespan = [p[1].linespan[0], p[2].linespan[1]]
+    linespan = (p[1].linespan[0], p[2].linespan[1])
     p[1].add(p[2])
     p[1].update_linespan(linespan)
     p[0] = p[1]
@@ -68,7 +68,7 @@ class Parser(Lexer):
 
   def p_section_wrapper(self, p):
     '''section_wrapper : BRACE_L section_opt BRACE_R'''
-    linespan = [p.linespan(1)[0], p.linespan(3)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(3)[1])
     p[2].update_linespan(linespan)
     p[0] = p[2]
 
@@ -86,14 +86,14 @@ class Parser(Lexer):
 
   def p_section_2(self, p):
     '''section : section sector'''
-    linespan = [p[1].linespan[0], p[2].linespan[1]]
+    linespan = (p[1].linespan[0], p[2].linespan[1])
     p[1].add(p[2])
     p[1].update_linespan(linespan)
     p[0] = p[1]
 
   def p_scopeless_section_wrapper(self, p):
     '''scopeless_section_wrapper : BRACE_L scopeless_section_opt BRACE_R'''
-    linespan = [p.linespan(1)[0], p.linespan(3)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(3)[1])
     p[2].update_linespan(linespan)
     p[0] = p[2]
 
@@ -111,7 +111,7 @@ class Parser(Lexer):
 
   def p_scopeless_section_2(self, p):
     '''scopeless_section : scopeless_section sector'''
-    linespan = [p.linespan(1)[0], p.linespan(2)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(2)[1])
     p[1].add(p[2])
     p[1].update_linespan(linespan)
     p[0] = p[1]
@@ -131,7 +131,7 @@ class Parser(Lexer):
   def p_function_declaration(self, p):
     '''function_declaration : type function_declarator section_wrapper'''
     p[2].add_type(p[1])
-    linespan = [p[1].linespan[0], p[3].linespan[1]]
+    linespan = (p[1].linespan[0], p[3].linespan[1])
     p[0] = ast.FnDeclaration(p[2], p[3], linespan=linespan)
 
   def p_function_declarator_1(self, p):
@@ -140,24 +140,24 @@ class Parser(Lexer):
 
   def p_function_declarator_2(self, p):
     '''function_declarator : ASTERISK function_declarator'''
-    linespan = [p.linespan(1)[0], p[2].linespan[1]]
+    linespan = (p.linespan(1)[0], p[2].linespan[1])
     p[2].add_type(ast.TypeNode(p[1], linespan=linespan))
     p[0] = p[2]
 
   def p_function_simple_declarator_1(self, p):
     '''function_simple_declarator : ID PAREN_L parameter_group PAREN_R'''
-    linespan = [p.linespan(1)[0], p.linespan(4)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(4)[1])
     p[0] = ast.FnDeclarator(p[1], p[3], linespan=linespan)
 
   def p_function_simple_declarator_2(self, p):
     '''function_simple_declarator : ID PAREN_L PAREN_R
                                   | ID PAREN_L VOID PAREN_R'''
-    linespan = [p.linespan(1)[0], p.linespan(len(p) - 1)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(len(p) - 1)[1])
     p[0] = ast.FnDeclarator(p[1], ast.EmptyNode(), linespan=linespan)
 
   def p_variable_declaration(self, p):
     '''variable_declaration : type variable_declarator_group SEMICOLON'''
-    linespan = [p[1].linespan[0], p.linespan(3)[1]]
+    linespan = (p[1].linespan[0], p.linespan(3)[1])
     vdl = ast.VaDeclationList(linespan=linespan)
     for variable_declarator in p[2]:
       variable_declarator.add_type(p[1])
@@ -179,7 +179,7 @@ class Parser(Lexer):
 
   def p_variable_declarator_2(self, p):
     '''variable_declarator : ASTERISK variable_declarator'''
-    linespan = [p.linespan(1)[0], p[2].linespan[1]]
+    linespan = (p.linespan(1)[0], p[2].linespan[1])
     p[2].add_type(ast.TypeNode(p[1], linespan=linespan))
     p[0] = p[2]
 
@@ -189,18 +189,18 @@ class Parser(Lexer):
 
   def p_variable_simple_declarator_2(self, p):
     '''variable_simple_declarator : ID BRACKET_L NUMBER_INT BRACKET_R'''
-    linespan = [p.linespan(1)[0], p.linespan(4)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(4)[1])
     p[0] = ast.ArrayDeclarator(p[1], p[3], linespan=linespan)
 
   def p_parameter_group_1(self, p):
     '''parameter_group : type variable_declarator'''
     p[2].add_type(p[1])
-    linespan = [p[1].linespan[0], p[2].linespan[1]]
+    linespan = (p[1].linespan[0], p[2].linespan[1])
     p[0] = ast.ParameterGroup(p[2], linespan=linespan)
 
   def p_parameter_group_2(self, p):
     '''parameter_group : parameter_group COMMA type variable_declarator'''
-    linespan = [p[1].linespan[0], p[4].linespan[1]]
+    linespan = (p[1].linespan[0], p[4].linespan[1])
     p[4].add_type(p[3])
     p[1].add(p[4])
     p[1].update_linespan(linespan)
@@ -217,48 +217,48 @@ class Parser(Lexer):
 
   def p_expression_statement(self, p):
     '''expression_statement : expression SEMICOLON'''
-    linespan = [p[1].linespan[0], p.linespan(2)[1]]
+    linespan = (p[1].linespan[0], p.linespan(2)[1])
     p[1].update_linespan(linespan)
     p[0] = p[1]
 
   def p_conditional_statement_1(self, p):
     '''conditional_statement : IF PAREN_L expression PAREN_R section_wrapper %prec IFS'''
-    linespan = [p.linespan(1)[0], p[5].linespan[1]]
+    linespan = (p.linespan(1)[0], p[5].linespan[1])
     p[0] = ast.ConditionalStatement(p[3], p[5], ast.EmptyNode(), linespan=linespan)
 
   def p_conditional_statement_2(self, p):
     '''conditional_statement : IF PAREN_L expression PAREN_R section_wrapper ELSE section_wrapper'''
-    linespan = [p.linespan(1)[0], p[7].linespan[1]]
+    linespan = (p.linespan(1)[0], p[7].linespan[1])
     p[0] = ast.ConditionalStatement(p[3], p[5], p[7], linespan=linespan)
 
   def p_loop_statement_1(self, p):
     '''loop_statement : WHILE PAREN_L expression PAREN_R section_wrapper'''
-    linespan = [p.linespan(1)[0], p[5].linespan[1]]
+    linespan = (p.linespan(1)[0], p[5].linespan[1])
     p[0] = ast.While(p[3], p[5], linespan=linespan)
 
   def p_loop_statement_2(self, p):
     '''loop_statement : FOR PAREN_L expression_statement expression_statement expression PAREN_R scopeless_section_wrapper'''
-    linespan = [p.linespan(1)[0], p[7].linespan[1]]
+    linespan = (p.linespan(1)[0], p[7].linespan[1])
     p[0] = ast.For(p[3], p[4], p[5], p[7], linespan=linespan)
 
   def p_jump_statement_1(self, p):
     '''jump_statement : RETURN SEMICOLON'''
-    linespan = [p.linespan(1)[0], p.linespan(2)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(2)[1])
     p[0] = ast.Return(ast.EmptyNode(), linespan=linespan)
 
   def p_jump_statement_2(self, p):
     '''jump_statement : RETURN expression SEMICOLON'''
-    linespan = [p.linespan(1)[0], p.linespan(3)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(3)[1])
     p[0] = ast.Return(p[2], linespan=linespan)
 
   def p_jump_statement_3(self, p):
     '''jump_statement : BREAK SEMICOLON'''
-    linespan = [p.linespan(1)[0], p.linespan(2)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(2)[1])
     p[0] = ast.Break(linespan=linespan)
 
   def p_jump_statement_4(self, p):
     '''jump_statement : CONTINUE SEMICOLON'''
-    linespan = [p.linespan(1)[0], p.linespan(2)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(2)[1])
     p[0] = ast.Continue(linespan=linespan)
 
   # Expression
@@ -269,7 +269,7 @@ class Parser(Lexer):
 
   def p_argument_expression_list_2(self, p):
     '''argument_expression_list : argument_expression_list COMMA expression'''
-    linespan = [p[1].linespan[0], p[3].linespan[1]]
+    linespan = (p[1].linespan[0], p[3].linespan[1])
     p[1].add(p[3])
     p[1].update_linespan(linespan)
     p[0] = p[1]
@@ -286,7 +286,7 @@ class Parser(Lexer):
                          | variable_expression ASSIGN_TIME expression
                          | variable_expression ASSIGN_DIVIDE expression
                          | variable_expression ASSIGN_MODULO expression'''
-    linespan = [p[1].linespan[0], p[3].linespan[1]]
+    linespan = (p[1].linespan[0], p[3].linespan[1])
     p[0] = ast.AssignOp(p[2], p[1], p[3], linespan=linespan)
 
   def p_additive_expression_1(self, p):
@@ -307,7 +307,7 @@ class Parser(Lexer):
                            | additive_expression ASTERISK unary_expression
                            | additive_expression DIVIDE unary_expression
                            | additive_expression MODULO unary_expression'''
-    linespan = [p[1].linespan[0], p[3].linespan[1]]
+    linespan = (p[1].linespan[0], p[3].linespan[1])
     p[0] = ast.BinaryOp(p[2], p[1], p[3], linespan=linespan)
 
   def p_unary_expression_1(self, p):
@@ -318,19 +318,19 @@ class Parser(Lexer):
   def p_unary_expression_2(self, p):
     '''unary_expression : MINUS unary_expression
                         | PLUS unary_expression'''
-    linespan = [p.linespan(1)[0], p[2].linespan[1]]
+    linespan = (p.linespan(1)[0], p[2].linespan[1])
     p[0] = ast.UnaryOp(p[1], p[2], linespan=linespan)
 
   def p_unary_expression_3(self, p):
     '''unary_expression : function_expression DOUBLE_PLUS
                         | function_expression DOUBLE_MINUS'''
-    linespan = [p[1].linespan[0], p.linespan(2)[1]]
+    linespan = (p[1].linespan[0], p.linespan(2)[1])
     p[0] = ast.BinaryOp(p[2], p[1], ast.Const(1), linespan=linespan)
 
   def p_unary_expression_4(self, p):
     '''unary_expression : DOUBLE_PLUS function_expression
                         | DOUBLE_MINUS function_expression'''
-    linespan = [p.linespan(1)[0], p[2].linespan[1]]
+    linespan = (p.linespan(1)[0], p[2].linespan[1])
     p[0] = ast.BinaryOp(p[1], p[2], ast.Const(1), linespan=linespan)
 
   def p_function_expression_1(self, p):
@@ -339,12 +339,12 @@ class Parser(Lexer):
 
   def p_function_expression_2(self, p):
     '''function_expression : function_expression PAREN_L argument_expression_list PAREN_R'''
-    linespan = [p[1].linespan[0], p.linespan(4)[1]]
+    linespan = (p[1].linespan[0], p.linespan(4)[1])
     p[0] = ast.FnExpression(p[1], p[3], linespan=linespan)
 
   def p_function_expression_3(self, p):
     '''function_expression : function_expression PAREN_L PAREN_R'''
-    linespan = [p[1].linespan[0], p.linespan(3)[1]]
+    linespan = (p[1].linespan[0], p.linespan(3)[1])
     p[0] = ast.FnExpression(p[1], ast.EmptyNode, linespan=linespan)
 
   def p_variable_expression_1(self, p):
@@ -354,7 +354,7 @@ class Parser(Lexer):
   def p_variable_expression_2(self, p):
     '''variable_expression : ASTERISK variable_simpe_expression
                            | AMPERSAND variable_simpe_expression'''
-    linespan = [p.linespan(1)[0], p[2].linespan[1]]
+    linespan = (p.linespan(1)[0], p[2].linespan[1])
     p[1].set_pointer(p[1])
     p[1].update_linespan(linespan)
     p[0] = p[1]
@@ -365,12 +365,12 @@ class Parser(Lexer):
 
   def p_variable_simpe_expression_2(self, p):
     '''variable_simpe_expression : variable_simpe_expression BRACKET_L expression BRACKET_R'''
-    linespan = [p[1].linespan[0], p.linespan(4)[1]]
+    linespan = (p[1].linespan[0], p.linespan(4)[1])
     p[0] = ast.ArrayExpression(p[1], p[3], linespan=linespan)
 
   def p_variable_simpe_expression_3(self, p):
     '''variable_simpe_expression : PAREN_L expression PAREN_R'''
-    linespan = [p.linespan(1)[0], p.linespan(3)[1]]
+    linespan = (p.linespan(1)[0], p.linespan(3)[1])
     p[2].update_linespan(linespan)
     p[0] = p[2]
 
